343f3ffc40618e1fd0b0173915e5115be5146703
Katariniss <echekina@gmail.com>
2019-12-02 18:03:17 +0500

    Adds the check of user title

--- src/components/user-title.js
+++ src/components/user-title.js
@@ -1,7 +1,18 @@
+const getUserTitle = (watchedFilmsNumber) => {
+  if (watchedFilmsNumber === 0) {
+    return ``;
+  } else if (watchedFilmsNumber >= 1 && watchedFilmsNumber <= 10) {
+    return `Novice`;
+  } else if (watchedFilmsNumber >= 11 && watchedFilmsNumber <= 20) {
+    return `Fan`;
+  }
+  return `Movie Buff`;
+};
+
 export const createUserTitleTemplate = () => {
   return (
     `<section class="header__profile profile">
-        <p class="profile__rating">Movie Buff</p>
+        <p class="profile__rating">${getUserTitle(21)}</p>
         <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
     </section>`
   );
