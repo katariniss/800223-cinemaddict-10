594d3a19f43ab03db4714cad8453badd34654e07
Katariniss <echekina@gmail.com>
2019-12-02 15:20:24 +0500

    Creates mock data for card and employs it in markup

--- src/main.js
+++ src/main.js
@@ -7,12 +7,10 @@ import {createShowMoreButtonTemplate} from './components/show-more.js';
 import {createSortingTemplate} from './components/sorting.js';
 import {createUserTitleTemplate} from './components/user-title.js';
 
+import {generateCard} from './mocks/card.js';
+
 const FILM_CARD_COUNT_IN_CARDLIST = 5;
 const FILM_CARD_COUNT_IN_EXTRA = 2;
-
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
 
 const siteHeaderElement = document.querySelector(`.header`);
 
@@ -26,26 +24,33 @@ render(siteMainElement, createSortingTemplate(), `beforeend`);
 render(siteMainElement, createFilmsListTemplate(), `beforeend`);
 
 const filmsElement = siteMainElement.querySelector(`.films`);
-const filmsListElement = filmsElement.querySelector(`.films-list`);
-const filmsListExtraElements = filmsElement.querySelectorAll(`.films-list--extra`);
 
-filmsListExtraElements.forEach((filmsListExtraElement) => {
-  const filmsListExtraElementContainer = filmsListExtraElement.querySelector(`.films-list__container`);
+renderExtraCards();
 
-  new Array(FILM_CARD_COUNT_IN_EXTRA)
-  .fill(``)
+function renderExtraCards() {
+  const filmsListExtraElements = filmsElement.querySelectorAll(`.films-list--extra`);
+
+  filmsListExtraElements.forEach((filmsListExtraElement) => {
+    renderGeneralCards(filmsListExtraElement, FILM_CARD_COUNT_IN_EXTRA);
+  });
+}
+
+const filmsGeneralListElement = filmsElement.querySelector(`.films-list`);
+
+renderGeneralCards(filmsGeneralListElement, FILM_CARD_COUNT_IN_CARDLIST);
+
+function renderGeneralCards(filmsListElement, numberOfCards) {
+  const filmsListContainerElement = filmsListElement.querySelector(`.films-list__container`);
+
+  [...new Array(numberOfCards)]
   .forEach(
-      () => render(filmsListExtraElementContainer, createFilmCardTemplate(), `beforeend`)
+      () => render(filmsListContainerElement, createFilmCardTemplate(generateCard()), `beforeend`)
   );
-});
+}
 
-const filmsListContainerElement = filmsListElement.querySelector(`.films-list__container`);
-
-new Array(FILM_CARD_COUNT_IN_CARDLIST)
-  .fill(``)
-  .forEach(
-      () => render(filmsListContainerElement, createFilmCardTemplate(), `beforeend`)
-  );
-
-render(filmsListElement, createShowMoreButtonTemplate(), `beforeend`);
+render(filmsGeneralListElement, createShowMoreButtonTemplate(), `beforeend`);
 render(siteMainElement, createFilmPopupTemplate(), `beforeend`);
+
+function render(container, template, place) {
+  container.insertAdjacentHTML(place, template);
+}
---
+++ src/mocks/card.js
@@ -0,0 +1,100 @@
+const FilmsList = [
+  `Matrix`,
+  `Green Book`,
+  `Frozen`,
+  `Brave Heart`,
+  `Good Will Hunting`,
+  `The Help`,
+  `Lilo and Stitch`,
+  `Adventureland`,
+  `Notting Hill`,
+  `Love, Actually`,
+  `Avengers`,
+  `Long Shot`,
+  `Crazy, Stupid, Love`,
+  `La-la-la Land`,
+  `Revenant`
+];
+
+const DescriptionItems = [
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+  `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+  `Fusce tristique felis at fermentum pharetra.`,
+  `Aliquam id orci ut lectus varius viverra.`,
+  `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+  `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+  `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+  `Aliquam erat volutpat.`,
+  `Nunc fermentum tortor ac porta dapibus.`,
+  `In rutrum ac purus sit amet tempus.`
+];
+
+const FilmsGenres = [
+  `comedy`,
+  `drama`,
+  `thriller`,
+  `animation`,
+  `musical`,
+  `fantasy`,
+  `science fiction`,
+  `romance`,
+  `action`
+];
+
+const FilmsPosters = [
+  `made-for-each-other.png`,
+  `popeye-meets-sinbad.png`,
+  `sagebrush-trail.jpg`,
+  `santa-claus-conquers-the-martians.jpg`,
+  `the-dance-of-life.jpg`,
+  `the-great-flamarion.jpg`,
+  `the-man-with-the-golden-arm.jpg`
+];
+
+const generateCards = (count) => {
+  return [...new Array(count)]
+    .map(generateCard);
+};
+
+const MAX_DURATION_IN_HOURS = 2;
+const MAX_DURATION_IN_MINUTES = 55;
+
+const MIN_YEAR = 1980;
+const MAX_YEAR = 2020;
+
+const MIN_RATING = 0;
+const MAX_RATING = 10;
+
+const generateCard = () => {
+  const DurationInHours = getRandomIntegerNumber(1, MAX_DURATION_IN_HOURS);
+  const DurationInMinutes = getRandomIntegerNumber(0, MAX_DURATION_IN_MINUTES);
+
+  return {
+    name: getRandomArrayItem(FilmsList),
+    description: getRandomArrayItem(DescriptionItems) + getRandomArrayItem(DescriptionItems),
+    rating: getRandomDecimal(MIN_RATING, MAX_RATING),
+    year: getRandomIntegerNumber(MIN_YEAR, (MAX_YEAR - MIN_YEAR)),
+    durationInHours: DurationInHours,
+    durationInMinutes: DurationInMinutes,
+    totalDuration: `${DurationInHours}h ${DurationInMinutes}m`,
+    genre: getRandomArrayItem(FilmsGenres),
+    poster: getRandomArrayItem(FilmsPosters),
+  };
+};
+
+const getRandomArrayItem = (array) => {
+  const randomIndex = getRandomIntegerNumber(0, array.length);
+
+  return array[randomIndex];
+};
+
+const getRandomIntegerNumber = (min, max) => {
+  return min + Math.floor(max * Math.random());
+};
+
+const getRandomDecimal = (min, max) => {
+  return ((max - min) * Math.random()).toFixed(1);
+};
+
+export {generateCard, generateCards};
